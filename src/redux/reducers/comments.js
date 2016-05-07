092d6047d609a4876a2a549815562998daea623a
Alex Olshansky <i@creagenics.com>
2016-05-07 12:56:49 +0200

    [reducers-relocation] Move "comments"

---
+++ src/redux/reducers/comments.js
@@ -0,0 +1,40 @@
+import _ from 'lodash';
+
+import * as ActionTypes from '../action-types';
+import * as ActionHelpers from '../action-helpers';
+
+const {response} = ActionHelpers;
+const indexById = list => _.keyBy(list || [], 'id');
+const mergeByIds = (state, array) => ({...state, ...indexById(array)});
+
+export default function comments(state = {}, action) {
+  if (ActionHelpers.isFeedResponse(action)) {
+    return mergeByIds(state, action.payload.comments);
+  }
+  switch (action.type) {
+    case response(ActionTypes.GET_SINGLE_POST):
+    case response(ActionTypes.SHOW_MORE_COMMENTS):
+    case response(ActionTypes.SHOW_MORE_LIKES_ASYNC): {
+      return mergeByIds(state, action.payload.comments);
+    }
+    case response(ActionTypes.SAVE_EDITING_COMMENT): {
+      return {...state, [action.payload.comments.id]: {...state[action.payload.comments.id], ...action.payload.comments}};
+    }
+    case response(ActionTypes.DELETE_COMMENT): {
+      return {...state, [action.request.commentId]: undefined};
+    }
+    case ActionTypes.REALTIME_COMMENT_NEW:
+    case ActionTypes.REALTIME_COMMENT_UPDATE: {
+      return mergeByIds(state, [action.comment]);
+    }
+    case ActionTypes.REALTIME_COMMENT_DESTROY: {
+      return {...state, [action.commentId] : undefined};
+    }
+    case response(ActionTypes.ADD_COMMENT): {
+      return {...state,
+        [action.payload.comments.id] : action.payload.comments
+      };
+    }
+  }
+  return state;
+}
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -707,48 +707,6 @@ export function attachments(state = {}, action) {
   return state;
 }
 
-function updateCommentData(state, action) {
-  return mergeByIds(state, action.payload.comments);
-}
-
-export function comments(state = {}, action) {
-  if (ActionHelpers.isFeedResponse(action)) {
-    return updateCommentData(state, action);
-  }
-  switch (action.type) {
-    case response(ActionTypes.SHOW_MORE_COMMENTS): {
-      return updateCommentData(state, action);
-    }
-    case response(ActionTypes.GET_SINGLE_POST): {
-      return updateCommentData(state, action);
-    }
-    case response(ActionTypes.SHOW_MORE_LIKES_ASYNC): {
-      return updateCommentData(state, action);
-    }
-    case response(ActionTypes.SAVE_EDITING_COMMENT): {
-      return {...state, [action.payload.comments.id]: {...state[action.payload.comments.id], ...action.payload.comments}};
-    }
-    case response(ActionTypes.DELETE_COMMENT): {
-      return {...state, [action.request.commentId] : undefined};
-    }
-    case ActionTypes.REALTIME_COMMENT_NEW: {
-      return mergeByIds(state, [action.comment]);
-    }
-    case ActionTypes.REALTIME_COMMENT_UPDATE: {
-      return mergeByIds(state, [action.comment]);
-    }
-    case ActionTypes.REALTIME_COMMENT_DESTROY: {
-      return {...state, [action.commentId] : undefined};
-    }
-    case response(ActionTypes.ADD_COMMENT): {
-      return {...state,
-        [action.payload.comments.id] : action.payload.comments
-      };
-    }
-  }
-  return state;
-}
-
 const COMMENT_SAVE_ERROR = 'Something went wrong while saving comment';
 
 function updateCommentViewState(state, action) {
@@ -1537,10 +1495,12 @@ export function sidebarViewState(state={}, action) {
   return state;
 }
 
+import comments from './comments';
 import posts from './posts';
 import users from './users';
 
 export {
+  comments,
   posts,
   users
 };
