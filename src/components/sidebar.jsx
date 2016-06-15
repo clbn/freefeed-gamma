b478f7c0c8e8d5d0a2d693cde1c757bc49ccdce4
Alex Olshansky <i@creagenics.com>
2016-06-15 21:53:37 +0200

    Remove "Coin Jar" from sidebar

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -84,49 +84,6 @@ export default ({user, signOut, recentGroups, toggleSidebar}) => (
           to&nbsp;your toolbar.
         </div>
       </div>
-
-      <div className='box'>
-        <div className='box-header-groups'>
-          Coin Jar
-        </div>
-        <div className='box-footer'>
-          <p style={{marginBottom: '10px'}}>Subscribe to Freefeed today! Arrangement is plain and simple — you wire funds to Freefeed, it gets better every week.</p>
-          <span style={{display: 'block', marginLeft: 'auto', marginRight: 'auto'}}>
-            <form action='https://www.paypal.com/cgi-bin/webscr' method='post' target='_top'>
-              <input type='hidden' name='cmd' value='_s-xclick'/>
-              <input type='hidden' name='hosted_button_id' value='DRR5XU73QLD7Y'/>
-              <table>
-                <tbody>
-                  <tr><td style={{paddingBottom: '5px'}}>
-                    <input type='hidden' name='on0' value='Pick monthly donation amount' style={{padding:'5px 0'}}/>Choose your option:
-                  </td></tr>
-                  <tr><td><select name='os0' defaultValue='Advanced'>
-                    <option value='Basic'>€5.00 EUR / month</option>
-                    <option value='Advanced'>€10.00 EUR / month</option>
-                    <option value='Sizable'>€15.00 EUR / month</option>
-                    <option value='Luxurious'>€20.00 EUR / month</option>
-                    <option value='King size'>€30.00 EUR / month</option>
-                    <option value='Master of the Universe'>€50.00 EUR / month</option>
-                    <option value='Chuck Norris'>€75.00 EUR / month</option>
-                    <option value='Duke Nukem'>€100.00 EUR / month</option>
-                  </select></td></tr>
-                </tbody>
-              </table>
-
-              <input type='hidden' name='currency_code' value='EUR'/>
-              <input type='image' src='https://www.paypalobjects.com/webstatic/en_US/i/buttons/PP_logo_h_100x26.png' border='0' name='submit' alt='PayPal - The safer, easier way to pay online!' style={{margin:'5px'}}/>
-              <img alt='' border='0' src='https://www.paypalobjects.com/en_US/i/scr/pixel.gif' width='1' height='1' style={{display: 'none !important'}}/>
-            </form>
-          </span>
-          <form action='https://www.paypal.com/cgi-bin/webscr' method='post' target='_top' id='singlePayPalPayment'>
-            <input type='hidden' name='cmd' value='_s-xclick'/>
-            <input type='hidden' name='hosted_button_id' value='HMVYD6GEWNWH8'/>
-            <input type='image' src='https://www.paypalobjects.com/webstatic/en_US/i/buttons/PP_logo_h_100x26.png' width='0' height='0' border='0' name='submit' alt='PayPal - The safer, easier way to pay online!'/>
-            <img alt='' border='0' src='https://www.paypalobjects.com/en_US/i/scr/pixel.gif' width='1' height='1' style={{display: 'none !important'}}/>
-          </form>
-          <p style={{marginBottom: '10px'}}>Or <a href='#' onclick='document.forms["singlePayPalPayment"].submit(); return false;' style={{textDecoration:'underline'}}>send a one-time payment first&nbsp;→</a></p>
-        </div>
-      </div>
     </div>
   </div>
 );
