a27c98cd75bc303b407b788208fb59a59b5f6873
Alex Olshansky <i@creagenics.com>
2016-07-05 23:40:21 +0200

    Add a throbber for "sending sub request" in UserCard

--- src/redux/reducers/user-views.js
+++ src/redux/reducers/user-views.js
@@ -6,14 +6,17 @@ const {request, response, fail} = ActionHelpers;
 export default function userViews(state = {}, action) {
   switch (action.type) {
     case request(ActionTypes.SUBSCRIBE):
+    case request(ActionTypes.SEND_SUBSCRIPTION_REQUEST):
     case request(ActionTypes.UNSUBSCRIBE): {
       const userId = action.payload.id;
       const userView = state[userId];
       return {...state, [userId]: {...userView, isSubscribing: true}};
     }
     case response(ActionTypes.SUBSCRIBE):
+    case response(ActionTypes.SEND_SUBSCRIPTION_REQUEST):
     case response(ActionTypes.UNSUBSCRIBE):
     case fail(ActionTypes.SUBSCRIBE):
+    case fail(ActionTypes.SEND_SUBSCRIPTION_REQUEST):
     case fail(ActionTypes.UNSUBSCRIBE): {
       const userId = action.request.id;
       const userView = state[userId];
