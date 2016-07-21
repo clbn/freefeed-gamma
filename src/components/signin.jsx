c04501b81a81d06b9f26824130b4749ca818308e
Alex Olshansky <i@creagenics.com>
2016-07-21 23:12:47 +0200

    Update layout in SignIn

--- src/components/signin.jsx
+++ src/components/signin.jsx
@@ -4,6 +4,48 @@ import {connect} from 'react-redux';
 import {signInChange, signIn, signInEmpty} from '../redux/action-creators';
 import {preventDefault} from '../utils';
 
+function signInFunc(props) {
+  if (props.username && props.password) {
+    props.signIn(props.username, props.password);
+  } else {
+    props.signInEmpty();
+  }
+}
+
+function SignIn(props) {
+  return (
+    <div className="box">
+      <div className="row">
+        <div className="col-md-6">
+          <h2>Sign in</h2>
+
+          {props.error ? (
+            <div className="alert alert-danger" role="alert">
+              {props.error}
+            </div>
+          ) : false}
+
+          <form onSubmit={preventDefault(() => signInFunc(props))}>
+            <div className="form-group">
+              <label htmlFor="username">Username</label>
+              <input className="form-control" type="text" onChange={e => props.signInChange(e.target.value)}/>
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input className="form-control" type="password" onChange={e => props.signInChange(undefined, e.target.value)}/>
+            </div>
+            <div className="form-group">
+              <button className="btn btn-default" type="submit">Sign in</button>
+            </div>
+          </form>
+
+          <p>New to FreeFeed? <Link to="/signup">Create an account »</Link></p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
 function mapStateToProps(state) {
   return {...state.signInForm};
 }
@@ -16,50 +58,4 @@ function mapDispatchToProps(dispatch) {
   };
 }
 
-function signInFunc(props) {
-  if (props.username && props.password) {
-    props.signIn(props.username, props.password);
-  } else {
-    props.signInEmpty();
-  }
-}
-
-function Signin(props) {
-  return (
-  <div className='box'>
-    <div className='box-header-timeline'>
-      Hello
-    </div>
-    <div className='box-body'>
-      <div className='col-md-12'>
-        <h2 className='p-signin-header'>Sign in</h2>
-        {props.error ? (<div className='alert alert-danger p-signin-error' role='alert'>
-                          <span id='error-message'>{props.error}</span>
-                        </div>) : false}
-        <div className='row'>
-          <div className='col-md-6'>
-              <form onSubmit={preventDefault(() => signInFunc(props))} className='p-signin'>
-                <div className='form-group'>
-                  <label htmlFor='username'>Username</label>
-                  <input id='username' className='ember-view ember-text-field form-control' type='text' onChange={e => props.signInChange(e.target.value)}/>
-                </div>
-                <div className='form-group'>
-                  <label htmlFor='password'>Password</label>
-                  <input id='password' className='ember-view ember-text-field form-control' type='password' onChange={e => props.signInChange(undefined, e.target.value)}/>
-                </div>
-                <div className='form-group'>
-                  <button className='btn btn-default p-singin-action' type='submit'>Sign in</button>
-                </div>
-              </form>
-              
-              <p>New to freefeed? <Link to='/signup'>Create an account »</Link></p>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div className='box-footer'>
-    </div>
-  </div>
-);}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Signin);
+export default connect(mapStateToProps, mapDispatchToProps)(SignIn);
