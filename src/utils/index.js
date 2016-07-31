7093a6942bbb6c447ed277253bf4829c372ec9da
Alex Olshansky <i@creagenics.com>
2016-07-31 22:57:15 +0200

    Update preventDefault() to deal with "fancy clicks" properly

--- src/utils/index.js
+++ src/utils/index.js
@@ -70,6 +70,14 @@ export function postParser(post) {
 
 export function preventDefault(realFunction) {
   return (event) => {
+    // Check if it's a "click" (not "submit", "keydown" etc).
+    // Also, check if it's a "fancy" one (made with middle button, or with Shift pressed etc).
+    // Then, if it's a "fancy click", don't prevent default action.
+    // Prevent custom action ("realFunction") instead.
+    if (event.type === 'click' && (event.button !== 0 || event.shiftKey || event.ctrlKey || event.altKey || event.metaKey)) {
+      return true;
+    }
+
     event.preventDefault();
     return realFunction && realFunction(event);
   };
