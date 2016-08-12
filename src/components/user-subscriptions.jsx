38ad1b86b7146a06e4df52916fab6cec8f0f39ac
Alex Olshansky <i@creagenics.com>
2016-08-12 19:01:23 +0200

    Add sorting to /username/subscriptions page (for owner only)

--- src/components/user-subscriptions.jsx
+++ src/components/user-subscriptions.jsx
@@ -25,16 +25,39 @@ const UserSubscriptions = (props) => {
       {props.isLoading ? (
         <img width="100" height="100" src={throbber100}/>
       ) : (
-        <TileList users={props.users}/>
+        <TileList users={props.users} sorting={props.sorting}/>
       )}
     </div>
   );
 };
 
-function mapStateToProps(state) {
+function mapStateToProps(state, ownProps) {
+  const isItMe = (state.user.username === ownProps.params.userName);
+  let users = [];
+  let sorting = null;
+
+  if (isItMe) {
+    // Subscriptions are the list of IDs, not users.
+    // Subscriptions are in chronological order, so we need to reverse them.
+    users = (state.user.subscriptions || [])
+      .map((id) => state.users[id] || {})
+      .filter((u) => !!u.type)
+      .reverse();
+
+    sorting = [
+      {'key': null, 'label': 'date you subscribed (most recent first)'},
+      {'key': 'username', 'label': 'username'},
+      {'key': 'screenName', 'label': 'display name'}
+    ];
+  } else {
+    users = _.sortBy(state.usernameSubscriptions.payload, 'username');
+  }
+
   return {
-    users: _.sortBy(state.usernameSubscriptions.payload, 'username'),
-    isLoading: state.usernameSubscriptions.isPending
+    isItMe,
+    users,
+    sorting,
+    isLoading: !isItMe && state.usernameSubscriptions.isPending
   };
 }
 
