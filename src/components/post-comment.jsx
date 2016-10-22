db182c9336c349da6f8f446b06e6eaf38f5172dc
Alex Olshansky <i@creagenics.com>
2016-10-22 21:04:58 +0200

    Fix linter errors in PostComment

--- src/components/post-comment.jsx
+++ src/components/post-comment.jsx
@@ -43,68 +43,67 @@ class PostComment extends React.Component {
     const createdAgo = fromNowOrNow(+this.props.createdAt) + '\n' + getFullDate(+this.props.createdAt);
 
     return (
-    <div className={`comment ${this.props.highlighted ? 'highlighted' : ''}`}>
-      <a className={iconClasses}
-         title={createdAgo}
-         id={`comment-${this.props.id}`}
-         href={`${this.props.postUrl}#comment-${this.props.id}`}
-         onClick={preventDefault(this.openAnsweringComment)}>
-        <i className="fa fa-comment fa-stack-1x"></i>
-        <i className="fa fa-comment-o fa-stack-1x"></i>
-      </a>
-      {this.props.isEditing ? (
-        <div className="comment-body">
-          <div>
-            <Textarea
-              ref="commentText"
-              className="comment-textarea"
-              defaultValue={this.props.body}
-              autoFocus={true}
-              onKeyDown={this.checkSave}
-              style={{ overflow: 'hidden', wordWrap: 'break-word' }}
-              minRows={2}
-              maxRows={10}
-              maxLength="1500"/>
-          </div>
-          <span>
-            <button className="btn btn-default btn-xs comment-post" onClick={this.saveComment}>Post</button>
-            <a className="comment-cancel" onClick={preventDefault(_=>this.props.toggleEditingComment(this.props.id))}>Cancel</a>
-          </span>
-          {this.props.isSaving ? (
-            <span className="comment-throbber">
-              <img width="16" height="16" src={throbber16}/>
-            </span>
-          ) : this.props.errorMessage ? (
-            <div className="comment-error alert alert-danger" role="alert">
-              Comment has not been saved. Server response: "{this.props.errorMessage}"
+      <div className={`comment ${this.props.highlighted ? 'highlighted' : ''}`}>
+        <a className={iconClasses}
+           title={createdAgo}
+           id={`comment-${this.props.id}`}
+           href={`${this.props.postUrl}#comment-${this.props.id}`}
+           onClick={preventDefault(this.openAnsweringComment)}>
+          <i className="fa fa-comment fa-stack-1x"></i>
+          <i className="fa fa-comment-o fa-stack-1x"></i>
+        </a>
+        {this.props.isEditing ? (
+          <div className="comment-body">
+            <div>
+              <Textarea
+                ref="commentText"
+                className="comment-textarea"
+                defaultValue={this.props.body}
+                autoFocus={true}
+                onKeyDown={this.checkSave}
+                style={{ overflow: 'hidden', wordWrap: 'break-word' }}
+                minRows={2}
+                maxRows={10}
+                maxLength="1500"/>
             </div>
-          ) : false}
-        </div>
-      ) : (
-        <div className="comment-body">
-          <PieceOfText
-            text={this.props.body}
-            userHover=  {{hover: username => this.props.highlightComment(username),
-                          leave: this.props.clearHighlightComment}}
-            arrowHover= {{hover: arrows => this.props.highlightArrowComment(arrows),
-                          leave: this.props.clearHighlightComment}}/>
-          {' -'}&nbsp;
-          <UserName user={this.props.user}/>
-          {this.props.isEditable ? (
             <span>
-              {' '}(<a onClick={preventDefault(_=>this.props.toggleEditingComment(this.props.id))}>edit</a>
-              &nbsp;|&nbsp;
-              <a onClick={confirmFirst(_=>this.props.deleteComment(this.props.id))}>delete</a>)
+              <button className="btn btn-default btn-xs comment-post" onClick={this.saveComment}>Post</button>
+              <a className="comment-cancel" onClick={preventDefault(_=>this.props.toggleEditingComment(this.props.id))}>Cancel</a>
             </span>
-          ) : (this.props.isDeletable && this.props.isModeratingComments) ? (
-            <span>
-              {' '}(<a onClick={confirmFirst(_=>this.props.deleteComment(this.props.id))}>delete</a>)
-            </span>
-          ) : false}
-        </div>
-      )}
-    </div>
-  );}
+            {this.props.isSaving ? (
+              <span className="comment-throbber">
+                <img width="16" height="16" src={throbber16}/>
+              </span>
+            ) : this.props.errorMessage ? (
+              <div className="comment-error alert alert-danger" role="alert">
+                Comment has not been saved. Server response: "{this.props.errorMessage}"
+              </div>
+            ) : false}
+          </div>
+        ) : (
+          <div className="comment-body">
+            <PieceOfText
+              text={this.props.body}
+              userHover={{hover: username => this.props.highlightComment(username), leave: this.props.clearHighlightComment}}
+              arrowHover={{hover: arrows => this.props.highlightArrowComment(arrows), leave: this.props.clearHighlightComment}}/>
+            {' -'}&nbsp;
+            <UserName user={this.props.user}/>
+            {this.props.isEditable ? (
+              <span>
+                {' '}(<a onClick={preventDefault(_=>this.props.toggleEditingComment(this.props.id))}>edit</a>
+                &nbsp;|&nbsp;
+                <a onClick={confirmFirst(_=>this.props.deleteComment(this.props.id))}>delete</a>)
+              </span>
+            ) : (this.props.isDeletable && this.props.isModeratingComments) ? (
+              <span>
+                {' '}(<a onClick={confirmFirst(_=>this.props.deleteComment(this.props.id))}>delete</a>)
+              </span>
+            ) : false}
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = (state) => {
