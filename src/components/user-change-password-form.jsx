e1b9ec6678b74195777754c76aa0fb6eb08fab9d
Alex Olshansky <i@creagenics.com>
2016-10-22 20:49:26 +0200

    Update code style in UserChangePasswordForm

--- src/components/user-change-password-form.jsx
+++ src/components/user-change-password-form.jsx
@@ -1,44 +1,50 @@
 import React from 'react';
+
 import {preventDefault} from '../utils';
 
 export default class UserChangePasswordForm extends React.Component {
-  render() {
-    return (
-      <form onSubmit={preventDefault(this.updatePassword)}>
-        <h3>Change password</h3>
-        <div className='form-group p-settings-currentpassword'>
-          <label htmlFor='currentPassword'>Current password:</label>
-          <input id='currentPassword' className='form-control' ref='currentPassword' type='password'/>
-        </div>
-        <div className='form-group p-settings-newpassword'>
-          <label htmlFor='password'>New password:</label>
-          <input id='password' className='form-control' ref='password' type='password'/>
-        </div>
-
-        <div className='form-group p-settings-confirmpassword'>
-          <label htmlFor='passwordConfirmation'>Confirm password:</label>
-          <input id='passwordConfirmation' className='form-control' ref='passwordConfirmation' type='password'/>
-        </div>
-        <p>
-          <button className='btn btn-default p-settings-updatepassword' type='submit'>Update Password</button>
-        </p>
-        {this.props.success ?
-          (<div className='alert alert-info p-settings-alert' role='alert'>
-              <span id='error-message'>Your password has been changed</span>
-            </div>) : false}
-        {this.props.error ?
-          (<div className='alert alert-danger p-settings-alert' role='alert'>
-              <span id='error-message'>{this.props.errorText}</span>
-            </div>) : false}
-      </form>);
-  }
   updatePassword = () => {
     if (!this.props.isSaving) {
       this.props.updatePassword({
         currentPassword: this.refs.currentPassword.value,
         password: this.refs.password.value,
-        passwordConfirmation: this.refs.passwordConfirmation.value,
+        passwordConfirmation: this.refs.passwordConfirmation.value
       });
     }
+  }
+
+  render() {
+    return (
+      <form onSubmit={preventDefault(this.updatePassword)}>
+        <h3>Change password</h3>
+
+        <div className="form-group">
+          <label htmlFor="currentPassword">Current password:</label>
+          <input id="currentPassword" className="form-control" ref="currentPassword" type="password"/>
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="password">New password:</label>
+          <input id="password" className="form-control" ref="password" type="password"/>
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="passwordConfirmation">Confirm password:</label>
+          <input id="passwordConfirmation" className="form-control" ref="passwordConfirmation" type="password"/>
+        </div>
+
+        <p>
+          <button className="btn btn-default" type="submit">Update Password</button>
+        </p>
+
+        {this.props.success ? (
+          <div className="alert alert-info" role="alert">Your password has been changed</div>
+        ) : false}
+
+        {this.props.error ? (
+          <div className="alert alert-danger" role="alert">{this.props.errorText}</div>
+        ) : false}
+      </form>
+    );
   }
 }
