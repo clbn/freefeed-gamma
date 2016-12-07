a19f8698dea586060a0ad71bb68de2eaae18901f
Alex Olshansky <i@creagenics.com>
2016-12-07 23:08:19 +0100

    Tidy up configure-store.js (middlewares et al.)

--- src/redux/configure-store.js
+++ src/redux/configure-store.js
@@ -1,24 +1,33 @@
-import {compose, createStore, applyMiddleware, combineReducers} from 'redux';
-
-import {apiMiddleware, authMiddleware, highlightedCommentsMiddleware, likesLogicMiddleware, userPhotoLogicMiddleware, groupPictureLogicMiddleware, redirectionMiddleware, directsMiddleware, realtimeMiddleware} from './middlewares';
+import {applyMiddleware, compose, createStore, combineReducers} from 'redux';
 import {routerReducer} from 'react-router-redux';
+
+import * as middlewares from './middlewares';
 import * as reducers from './reducers';
 
-//order matters — we need to stop unauthed async fetching before request, see authMiddleware
-const middleware = [ authMiddleware, apiMiddleware, highlightedCommentsMiddleware, likesLogicMiddleware, userPhotoLogicMiddleware, groupPictureLogicMiddleware, redirectionMiddleware, directsMiddleware, realtimeMiddleware ];
+// Order matters: we need to check for authentication before API calls
+const middlewareChain = [
+  middlewares.authMiddleware,
+  middlewares.apiMiddleware,
+  middlewares.highlightedCommentsMiddleware,
+  middlewares.likesLogicMiddleware,
+  middlewares.userPhotoLogicMiddleware,
+  middlewares.groupPictureLogicMiddleware,
+  middlewares.redirectionMiddleware,
+  middlewares.directsMiddleware,
+  middlewares.realtimeMiddleware
+];
 
+let enhancers = [applyMiddleware(...middlewareChain)];
+
+// Tell webpack to include devtool enhancer in dev mode
 const isDevelopment = process.env.NODE_ENV != 'production';
-
-let enhancers = [applyMiddleware(...middleware),];
-
-//tells webpack to include devtool enhancer in dev mode
 if (isDevelopment && window.devToolsExtension) {
   enhancers.push(window.devToolsExtension());
 }
 
 const storeEnhancer = compose(...enhancers);
-
 const createStoreWithMiddleware = storeEnhancer(createStore);
+
 const reducer = combineReducers({...reducers, routing: routerReducer});
 
 export default function configureStore(initialState) {
