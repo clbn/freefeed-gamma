213e18f18ddaaf80b8897acb436b56617f8c92e7
Alex Olshansky <i@creagenics.com>
2017-01-14 19:13:07 +0100

    [jsx-no-bind] Replace arrow function with prepared prop in RealtimeSwitch

--- src/components/elements/realtime-switch.jsx
+++ src/components/elements/realtime-switch.jsx
@@ -6,7 +6,7 @@ import { updateFrontendPreferences, home } from '../../redux/action-creators';
 const RealtimeSwitch = (props) => (
   <div className={'realtime-switch' + (props.frontendPreferences.realtimeActive ? ' on' : ' off')}>
     <div className="realtime-switch-label">Realtime updates</div>
-    <div className="realtime-switch-range" onClick={() => props.toggle(props.userId, props.frontendPreferences)}>
+    <div className="realtime-switch-range" onClick={props.handleClick}>
       <div className="realtime-switch-state">{props.frontendPreferences.realtimeActive ? 'on' : 'off'}</div>
       <div className="realtime-switch-toggle"></div>
     </div>
@@ -36,4 +36,11 @@ const mapDispatchToProps = (dispatch) => {
   };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(RealtimeSwitch);
+const mergeProps = (stateProps, dispatchProps) => {
+  return {
+    ...stateProps,
+    handleClick: () => dispatchProps.toggle(stateProps.userId, stateProps.frontendPreferences)
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps, mergeProps)(RealtimeSwitch);
