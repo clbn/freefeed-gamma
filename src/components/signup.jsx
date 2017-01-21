d39030fbe75ea3e3efadc388ca62b151413d3f83
Alex Olshansky <i@creagenics.com>
2017-01-21 19:26:22 +0100

    Refactor SignUp into non-simplified component

--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -5,56 +5,64 @@ import { preventDefault } from '../utils';
 import { captcha as captchaConfig } from '../config';
 import Recaptcha from 'react-google-recaptcha';
 
-function signUpFunc(props) {
-  props.signUp({ ...props });
-}
+class SignUp extends React.Component {
+  submitForm = () => {
+    const username = this.props.username;
+    const password = this.props.password;
+    const email = this.props.email;
+    const captcha = this.props.captcha;
+    this.props.signUp({ username, password, email, captcha });
+  };
 
-function SignUp(props) {
-  return (
-    <div className="box">
-      <div className="row">
-        <div className="col-md-6">
-          <h2>Sign up</h2>
+  render() {
+    const props = this.props;
 
-          <form onSubmit={preventDefault(() => signUpFunc(props))}>
-            <div className="form-group">
-              <label htmlFor="username-input">Username</label>
-              <input id="username-input" className="form-control" type="text" onChange={e => props.signUpChange({ username: e.target.value })}/>
-            </div>
+    return (
+      <div className="box">
+        <div className="row">
+          <div className="col-md-6">
+            <h2>Sign up</h2>
 
-            <div className="form-group">
-              <label htmlFor="email-input">Email</label>
-              <input id="email-input" className="form-control" type="email" onChange={e => props.signUpChange({ email: e.target.value })}/>
-            </div>
-
-            <div className="form-group">
-              <label htmlFor="password-input">Password</label>
-              <input id="password-input" className="form-control" type="password" onChange={e => props.signUpChange({ password: e.target.value })}/>
-            </div>
-
-            {captchaConfig.siteKey ? (
+            <form onSubmit={preventDefault(this.submitForm)}>
               <div className="form-group">
-                <Recaptcha
-                  sitekey={captchaConfig.siteKey}
-                  theme="light"
-                  type="image"
-                  onChange={v => props.signUpChange({ captcha: v })}
-                  onExpired={_ => props.signUpChange({ captcha: null })} />
+                <label htmlFor="username-input">Username</label>
+                <input id="username-input" className="form-control" type="text" onChange={e => props.signUpChange({ username: e.target.value })}/>
               </div>
+
+              <div className="form-group">
+                <label htmlFor="email-input">Email</label>
+                <input id="email-input" className="form-control" type="email" onChange={e => props.signUpChange({ email: e.target.value })}/>
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="password-input">Password</label>
+                <input id="password-input" className="form-control" type="password" onChange={e => props.signUpChange({ password: e.target.value })}/>
+              </div>
+
+              {captchaConfig.siteKey ? (
+                <div className="form-group">
+                  <Recaptcha
+                    sitekey={captchaConfig.siteKey}
+                    theme="light"
+                    type="image"
+                    onChange={v => props.signUpChange({ captcha: v })}
+                    onExpired={_ => props.signUpChange({ captcha: null })} />
+                </div>
+              ) : false}
+
+              <div className="form-group">
+                <button className="btn btn-default" type="submit">Sign up</button>
+              </div>
+            </form>
+
+            {props.error ? (
+              <div className="alert alert-danger" role="alert">{props.error}</div>
             ) : false}
-
-            <div className="form-group">
-              <button className="btn btn-default" type="submit">Sign up</button>
-            </div>
-          </form>
-
-          {props.error ? (
-            <div className="alert alert-danger" role="alert">{props.error}</div>
-          ) : false}
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 function mapStateToProps(state) {
