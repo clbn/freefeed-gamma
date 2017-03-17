clbn/freefeed-gamma

commit a6a9619d5a06794cc58eae78d9f6c3afdd78a0ec

[components-rewiring] PostCreateForm: connect props and actions directly
This eliminates a lot of duplicating code and simplifies data flow.

src/components/elements/user-profile.jsx
@@ -188,14 +188,7 @@ export default class UserProfile extends React.Component {
         ) : false}
 
         {props.canIPostHere ? (
-          <PostCreateForm
-            sendTo={props.sendTo}
-            user={props.user}
-            createPost={props.createPost}
-            resetPostCreateForm={props.resetPostCreateForm}
-            createPostForm={props.createPostForm}
-            addAttachmentResponse={props.addAttachmentResponse}
-            removeAttachment={props.removeAttachment}/>
+          <PostCreateForm/>
         ) : false}
       </div>
     );

src/components/user.jsx
@@ -2,9 +2,8 @@ import React from 'react';
 import { connect } from 'react-redux';
 import _ from 'lodash';
 
-import { createPost, resetPostCreateForm, addAttachmentResponse, removeAttachment } from '../redux/action-creators';
 import { getVisibleEntriesWithHidden } from '../redux/selectors';
-import { joinCreatePostData, userActions } from '../redux/select-utils';
+import { userActions } from '../redux/select-utils';
 import { getCurrentRouteName } from '../utils';
 import UserProfile from './elements/user-profile';
 import UserSubscribers from './elements/user-subscribers';
@@ -18,14 +17,7 @@ const User = (props) => {
       <UserProfile
         {...props.viewUser}
         {...props.userActions}
-        isInUserPostFeed={props.isInUserPostFeed}
-        user={props.user}
-        sendTo={props.sendTo}
-        createPost={props.createPost}
-        resetPostCreateForm={props.resetPostCreateForm}
-        createPostForm={props.createPostForm}
-        addAttachmentResponse={props.addAttachmentResponse}
-        removeAttachment={props.removeAttachment}/>
+        isInUserPostFeed={props.isInUserPostFeed}/>
 
       {props.viewUser.isUserFound ? (
         props.currentRoute === 'userSubscribers' ? (
@@ -48,7 +40,6 @@ function mapStateToProps(state, ownProps) {
   const user = state.user;
   const authenticated = state.authenticated;
   const visibleEntries = getVisibleEntriesWithHidden(state);
-  const createPostForm = joinCreatePostData(state);
   const boxHeader = state.boxHeader;
   const requestedUsername = ownProps.params.userName;
   const foundUser = Object.getOwnPropertyNames(state.users)
@@ -94,18 +85,12 @@ function mapStateToProps(state, ownProps) {
 
   const viewUser = { ...(foundUser), ...statusExtension };
 
-  const sendTo = { ...state.sendTo, defaultFeed: (foundUser ? foundUser.username : null) };
-
-  return { user, visibleEntries, createPostForm, boxHeader, viewUser, sendTo, currentRoute, isInUserPostFeed, showPaginationHeader };
+  return { visibleEntries, boxHeader, viewUser, currentRoute, isInUserPostFeed, showPaginationHeader };
 }
 
 function mapDispatchToProps(dispatch) {
   return {
-    userActions: userActions(dispatch),
-    createPost: (...args) => dispatch(createPost(...args)),
-    resetPostCreateForm: (...args) => dispatch(resetPostCreateForm(...args)),
-    addAttachmentResponse: (...args) => dispatch(addAttachmentResponse(...args)),
-    removeAttachment: (...args) => dispatch(removeAttachment(...args))
+    userActions: userActions(dispatch)
   };
 }
 

src/components/discussions.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import { createPost, resetPostCreateForm, addAttachmentResponse, removeAttachment } from '../redux/action-creators';
 import { getVisibleEntriesWithHidden } from '../redux/selectors';
-import { joinCreatePostData } from '../redux/select-utils';
 import { getCurrentRouteName } from '../utils';
 
 import PostCreateForm from './elements/post-create-form';
@@ -11,16 +9,7 @@ import Feed from './elements/feed';
 import PaginatedView from './elements/paginated-view';
 
 const Discussions = (props) => {
-  const createPostComponent = (
-    <PostCreateForm
-      sendTo={props.sendTo}
-      user={props.user}
-      createPost={props.createPost}
-      resetPostCreateForm={props.resetPostCreateForm}
-      createPostForm={props.createPostForm}
-      addAttachmentResponse={props.addAttachmentResponse}
-      removeAttachment={props.removeAttachment}/>
-  );
+  const postCreateForm = <PostCreateForm defaultRecipient={props.defaultRecipient}/>;
 
   return (
     <div className="box">
@@ -34,7 +23,7 @@ const Discussions = (props) => {
         ) : false}
       </div>
 
-      <PaginatedView firstPageHead={createPostComponent} {...props}>
+      <PaginatedView firstPageHead={postCreateForm} {...props}>
         <Feed {...props}/>
       </PaginatedView>
     </div>
@@ -43,26 +32,14 @@ const Discussions = (props) => {
 
 function mapStateToProps(state, ownProps) {
   const isLoading = state.routeLoadingState;
-  const user = state.user;
   const authenticated = state.authenticated;
   const visibleEntries = getVisibleEntriesWithHidden(state);
-  const createPostForm = joinCreatePostData(state);
   const boxHeader = state.boxHeader;
 
   const currentRoute = getCurrentRouteName(ownProps);
-  const defaultFeed = (currentRoute === 'discussions' ? user.username : null);
-  const sendTo = { ...state.sendTo, defaultFeed };
+  const defaultRecipient = (currentRoute === 'discussions' ? state.user.username : null);
 
-  return { isLoading, user, authenticated, visibleEntries, createPostForm, boxHeader, sendTo };
+  return { isLoading, authenticated, visibleEntries, boxHeader, defaultRecipient };
 }
 
-function mapDispatchToProps(dispatch) {
-  return {
-    createPost: (...args) => dispatch(createPost(...args)),
-    resetPostCreateForm: (...args) => dispatch(resetPostCreateForm(...args)),
-    addAttachmentResponse: (...args) => dispatch(addAttachmentResponse(...args)),
-    removeAttachment: (...args) => dispatch(removeAttachment(...args))
-  };
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Discussions);
+export default connect(mapStateToProps)(Discussions);

src/components/elements/post-create-form.jsx
@@ -8,13 +8,15 @@ import PostAttachments from './post-attachments';
 import { preventDefault } from '../../utils';
 import throbber16 from 'assets/images/throbber-16.gif';
 
+import { createPost, resetPostCreateForm, addAttachmentResponse, removeAttachment } from '../../redux/action-creators';
+import { joinCreatePostData } from '../../redux/select-utils';
+
 class PostCreateForm extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
       isExpanded: !!props.recipientFromUrl,
-      defaultFeed: props.recipientFromUrl || props.sendTo.defaultFeed,
       isFormEmpty: true,
       isMoreOpen: false,
       hasUploadFailed: false,
@@ -126,11 +128,11 @@ class PostCreateForm extends React.Component {
   }
 
   componentWillReceiveProps(newProps) {
-    // If defaultFeed gets updated, it means the transition between Direct messages
+    // If defaultRecipient gets updated, it means the transition between Direct messages
     // and Discussions pages happened (they share the top component, so PostCreateForm
     // doesn't get unmounted/mounted in the process). That's one "hacky" way to check
     // for this transition without passing another prop from discussions.jsx
-    if (newProps.sendTo.defaultFeed !== this.props.sendTo.defaultFeed) {
+    if (newProps.defaultRecipient !== this.props.defaultRecipient) {
       this.clearForm();
     }
 
@@ -173,7 +175,7 @@ class PostCreateForm extends React.Component {
       this.setState({ hasUploadFailed: true });
     };
 
-    const defaultFeed = this.props.recipientFromUrl || this.props.sendTo.defaultFeed;
+    const defaultFeed = this.props.recipientFromUrl || this.props.defaultRecipient;
 
     return (
       <div className={'create-post post-editor' + (this.state.isExpanded ? ' expanded' : '')}>
@@ -266,10 +268,23 @@ class PostCreateForm extends React.Component {
   }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = (state, ownProps) => {
   return {
+    createPostForm: joinCreatePostData(state),
+    user: state.user,
+    sendTo: state.sendTo,
+    defaultRecipient: (ownProps.defaultRecipient !== undefined ? ownProps.defaultRecipient : state.user.username),
     recipientFromUrl: state.routing.locationBeforeTransitions.query.to
   };
 };
 
-export default connect(mapStateToProps)(PostCreateForm);
+function mapDispatchToProps(dispatch) {
+  return {
+    createPost: (...args) => dispatch(createPost(...args)),
+    resetPostCreateForm: (...args) => dispatch(resetPostCreateForm(...args)),
+    addAttachmentResponse: (...args) => dispatch(addAttachmentResponse(...args)),
+    removeAttachment: (...args) => dispatch(removeAttachment(...args))
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(PostCreateForm);

src/components/home.jsx
@@ -2,9 +2,8 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router';
 
-import { createPost, resetPostCreateForm, addAttachmentResponse, removeAttachment, toggleHiddenPosts } from '../redux/action-creators';
+import { toggleHiddenPosts } from '../redux/action-creators';
 import { getVisibleEntriesWithHidden, getHiddenEntriesWithHidden } from '../redux/selectors';
-import { joinCreatePostData } from '../redux/select-utils';
 import { pluralForm } from '../utils';
 
 import PostCreateForm from './elements/post-create-form';
@@ -14,17 +13,6 @@ import RealtimeSwitch from './elements/realtime-switch';
 import Welcome from './elements/welcome';
 
 const Home = (props) => {
-  const createPostComponent = (
-    <PostCreateForm
-      sendTo={props.sendTo}
-      user={props.user}
-      createPost={props.createPost}
-      resetPostCreateForm={props.resetPostCreateForm}
-      createPostForm={props.createPostForm}
-      addAttachmentResponse={props.addAttachmentResponse}
-      removeAttachment={props.removeAttachment}/>
-  );
-
   const userRequestsCount = props.userRequestsCount;
   const groupRequestsCount = props.groupRequestsCount;
   const totalRequestsCount = userRequestsCount + groupRequestsCount;
@@ -32,6 +20,8 @@ const Home = (props) => {
   const userRequestsText = pluralForm(userRequestsCount, 'subscription request');
   const groupRequestsText = pluralForm(groupRequestsCount, 'group subscription request');
   const bothRequestsDisplayed = userRequestsCount > 0 && groupRequestsCount > 0;
+
+  const postCreateForm = <PostCreateForm/>;
 
   return (
     <div className="box">
@@ -64,7 +54,7 @@ const Home = (props) => {
             </div>
           </div>
 
-          <PaginatedView firstPageHead={createPostComponent} {...props}>
+          <PaginatedView firstPageHead={postCreateForm} {...props}>
             <Feed {...props} isInHomeFeed={true}/>
           </PaginatedView>
         </div>
@@ -78,16 +68,13 @@ const Home = (props) => {
 function mapStateToProps(state) {
   const isLoading = state.routeLoadingState;
 
-  const user = state.user;
   const authenticated = state.authenticated;
 
   const visibleEntries = getVisibleEntriesWithHidden(state);
   const hiddenEntries = getHiddenEntriesWithHidden(state);
   const isHiddenRevealed = state.feedViewState.isHiddenRevealed;
 
-  const createPostForm = joinCreatePostData(state);
   const boxHeader = state.boxHeader;
-  const sendTo = { ...state.sendTo, defaultFeed: user.username };
   const userRequestsCount = state.userRequests.length;
   const groupRequestsCount = state.groupRequests.length;
 
@@ -95,19 +82,15 @@ function mapStateToProps(state) {
 
   return {
     isLoading,
-    user, authenticated,
+    authenticated,
     visibleEntries, hiddenEntries, isHiddenRevealed,
-    createPostForm, boxHeader, sendTo, userRequestsCount, groupRequestsCount,
+    boxHeader, userRequestsCount, groupRequestsCount,
     offset
   };
 }
 
 function mapDispatchToProps(dispatch) {
   return {
-    createPost: (...args) => dispatch(createPost(...args)),
-    resetPostCreateForm: (...args) => dispatch(resetPostCreateForm(...args)),
-    addAttachmentResponse: (...args) => dispatch(addAttachmentResponse(...args)),
-    removeAttachment: (...args) => dispatch(removeAttachment(...args)),
     toggleHiddenPosts: () => dispatch(toggleHiddenPosts())
   };
 }

commit 581a66f7716120bc1688fae320b718731645b3d5

Fix default recipient in PostCreateForm on group page
The bug was introduced in a6a9619d5a06794cc58eae78d9f6c3afdd78a0ec.

src/components/elements/user-profile.jsx
@@ -188,7 +188,7 @@ export default class UserProfile extends React.Component {
         ) : false}
 
         {props.canIPostHere ? (
-          <PostCreateForm/>
+          <PostCreateForm defaultRecipient={props.defaultRecipient}/>
         ) : false}
       </div>
     );

src/components/user.jsx
@@ -17,7 +17,8 @@ const User = (props) => {
       <UserProfile
         {...props.viewUser}
         {...props.userActions}
-        isInUserPostFeed={props.isInUserPostFeed}/>
+        isInUserPostFeed={props.isInUserPostFeed}
+        defaultRecipient={props.defaultRecipient}/>
 
       {props.viewUser.isUserFound ? (
         props.currentRoute === 'userSubscribers' ? (
@@ -85,7 +86,9 @@ function mapStateToProps(state, ownProps) {
 
   const viewUser = { ...(foundUser), ...statusExtension };
 
-  return { visibleEntries, boxHeader, viewUser, currentRoute, isInUserPostFeed, showPaginationHeader };
+  const defaultRecipient = (foundUser ? foundUser.username : null);
+
+  return { visibleEntries, boxHeader, viewUser, currentRoute, isInUserPostFeed, showPaginationHeader, defaultRecipient };
 }
 
 function mapDispatchToProps(dispatch) {