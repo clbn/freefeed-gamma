d8b3d1260126625bcf94165197bc960f47143c5e
Alex Olshansky <i@creagenics.com>
2017-04-18 21:22:58 +0200

    Use new icon for Protected level in UserProfile

--- src/components/elements/user-feed-status.jsx
+++ src/components/elements/user-feed-status.jsx
@@ -5,7 +5,11 @@ const UserFeedStatus = (props) => (
     {props.isPrivate === '1' ? (
       <span><i className="fa fa-lock"></i> Private</span>
     ) : props.isProtected === '1' ? (
-      <span><i className="icon-protected"></i> Protected</span>
+      <span><i className="icon-protected">
+        <i className="icon-protected-bg fa fa-user"></i>
+        <i className="icon-protected-shadow fa fa-user-o fa-inverse"></i>
+        <i className="icon-protected-fg fa fa-user"></i>
+      </i> Protected</span>
     ) : (
       <span><i className="fa fa-globe"></i> Public</span>
     )}
