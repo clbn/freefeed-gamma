1c1c16c57b8ee240eadd1d5731b3ae1aadefbddf
Alex Olshansky <i@creagenics.com>
2017-12-10 01:04:20 +0100

    Refactor: clean up reducer "commentViews"

--- src/redux/reducers/comment-views.js
+++ src/redux/reducers/comment-views.js
@@ -5,75 +5,68 @@ import * as ActionHelpers from '../action-helpers';
 
 const { request, response, fail } = ActionHelpers;
 
+const updateRecord = (state, id, props) => {
+  return { ...state, [id]: { ...state[id], ...props } };
+};
+
+const deleteRecord = (state, id) => {
+  if (!state[id]) {
+    return state;
+  }
+  const newState = { ...state };
+  delete newState[id];
+  return newState;
+};
+
 export default function commentViews(state={}, action) {
   switch (action.type) {
     case ActionTypes.TOGGLE_EDITING_COMMENT: {
-      return {
-        ...state,
-        [action.commentId]: {
-          ...state[action.commentId],
-          isEditing: !state[action.commentId].isEditing,
-          errorMessage: ''
-        }
-      };
+      const id = action.commentId;
+      const currentValue = (state[id] || {}).isEditing;
+      return updateRecord(state, id, { isEditing: !currentValue, errorMessage: null });
     }
     case request(ActionTypes.SAVE_EDITING_COMMENT): {
-      return { ...state, [action.payload.commentId]: { ...state[action.payload.commentId], isSaving: true } };
+      const id = action.payload.commentId;
+      return updateRecord(state, id, { isSaving: true });
     }
     case response(ActionTypes.SAVE_EDITING_COMMENT): {
-      return { ...state, [action.payload.comments.id]: { ...state[action.payload.comments.id], isEditing: false, isSaving: false, errorMessage: '' } };
+      const id = action.payload.comments.id;
+      return updateRecord(state, id, { isEditing: false, isSaving: false, errorMessage: null });
     }
     case fail(ActionTypes.SAVE_EDITING_COMMENT): {
-      return { ...state, [action.request.commentId]: { ...state[action.request.commentId], isEditing: true, isSaving: false, errorMessage: (action.payload || {}).err } };
+      const id = action.request.commentId;
+      const error = (action.payload || {}).err;
+      return updateRecord(state, id, { isEditing: true, isSaving: false, errorMessage: error });
     }
     case response(ActionTypes.DELETE_COMMENT): {
-      const newState = { ...state };
-      delete newState[action.request.commentId];
-      return newState;
+      const id = action.request.commentId;
+      return deleteRecord(state, id);
     }
     case response(ActionTypes.ADD_COMMENT): {
       const id = action.payload.comments.id;
-      return { ...state,
-        [id]: { ...state[id],
-          isEditing: false
-        }
-      };
+      return updateRecord(state, id, { isEditing: false });
     }
 
     case request(ActionTypes.LIKE_COMMENT):
     case request(ActionTypes.UNLIKE_COMMENT): {
       const id = action.payload.commentId;
-      return { ...state,
-        [id]: { ...state[id],
-          isLiking: true
-        }
-      };
+      return updateRecord(state, id, { isLiking: true });
     }
     case response(ActionTypes.LIKE_COMMENT):
     case response(ActionTypes.UNLIKE_COMMENT): {
       const id = action.request.commentId;
-      return { ...state,
-        [id]: { ...state[id],
-          isLiking: false
-        }
-      };
+      return updateRecord(state, id, { isLiking: false, likeError: null });
     }
     case fail(ActionTypes.LIKE_COMMENT):
     case fail(ActionTypes.UNLIKE_COMMENT): {
       const id = action.request.commentId;
-      const errorString = 'Something went wrong while liking the comment...';
-      return { ...state,
-        [id]: { ...state[id],
-          isLiking: false,
-          likeError: errorString
-        }
-      };
+      const error = 'Something went wrong while liking the comment...';
+      return updateRecord(state, id, { isLiking: false, likeError: error });
     }
 
     case ActionTypes.REALTIME_COMMENT_DESTROY: {
-      const newState = { ...state };
-      delete newState[action.commentId];
-      return newState;
+      const id = action.commentId;
+      return deleteRecord(state, id);
     }
 
     case ActionTypes.UPDATE_HIGHLIGHTED_COMMENTS: {
