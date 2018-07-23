clbn/freefeed-gamma

commit d21c83fef9da4155e9472caa2c96feba1bf1dd37

summer-cleaning: Make Post selector more granular
Instead of taking the whole user, we only take username and profile pic.
And createdBy prop is not overridden by the object anymore and is
staying a string (user ID).

This way, all the page's posts don't get re-rendered on every
state.users update. Big success.

src/components/elements/post.jsx
@@ -147,11 +147,11 @@ class Post extends React.Component {
       'userpic': true,
       'userpic-large': props.isSinglePost
     });
-    const userpicImage = (props.isSinglePost ? props.createdBy.profilePictureLargeUrl : props.createdBy.profilePictureMediumUrl);
+    const userpicImage = (props.isSinglePost ? props.authorLargePic : props.authorMediumPic);
     const userpicSize = (props.isSinglePost ? 75 : 50);
     const primaryUserpic = (
       <div className={primaryUserpicClasses}>
-        <Link to={`/${props.createdBy.username}`}>
+        <Link to={`/${props.authorUsername}`}>
           <img src={userpicImage} width={userpicSize} height={userpicSize}/>
         </Link>
       </div>
@@ -162,7 +162,7 @@ class Post extends React.Component {
       'userpic-secondary': true,
       'userpic-large': props.isSinglePost
     });
-    const externalRecipients = props.recipients.filter((recipient) => (recipient.id !== props.createdBy.id));
+    const externalRecipients = props.recipients.filter((recipient) => (recipient.id !== props.createdBy));
     const hasSecondaryUserpics = (externalRecipients.length > 0);
     const secondaryUserpicSize = (props.isSinglePost ? 50 : 33);
     const secondaryUserpicInterval = (props.isSinglePost ? 9 : 6);
@@ -183,7 +183,7 @@ class Post extends React.Component {
 
     // Recipients
     const recipientCustomDisplay = function(recipient) {
-      if (recipient.id !== props.createdBy.id) {
+      if (recipient.id !== props.createdBy) {
         return false;
       }
       if (recipient.username[recipient.username.length - 1] === 's') {
@@ -204,7 +204,7 @@ class Post extends React.Component {
       } else {
         // When in a many-sources list (Home, Direct messages, My discussions),
         // we should omit the only recipient if it's the author's feed.
-        if (recipients[0].id === props.createdBy.id) {
+        if (recipients[0].id === props.createdBy) {
           recipients = [];
         }
       }
@@ -350,7 +350,7 @@ class Post extends React.Component {
 
         <div className="post-top">
           <div className="post-header">
-            <UserName className="post-author" id={props.createdBy.id}/>
+            <UserName className="post-author" id={props.createdBy}/>
             {recipients.length > 0 ? ' to ' : false}
             {recipients}
           </div>
@@ -425,7 +425,7 @@ class Post extends React.Component {
           <div className="dropzone-previews"></div>
 
           <div className="post-footer">
-            <PostVisibilityIcon recipients={props.recipients} authorId={props.createdBy.id}/>
+            <PostVisibilityIcon recipients={props.recipients} authorId={props.createdBy}/>
 
             <Link to={postUrl} className="post-timestamp">
               <time dateTime={dateISO} title={dateFull}>{dateRelative}</time>

src/redux/selectors/post.js
@@ -90,15 +90,25 @@ const makeGetPost = () => createSelector(
   [
     (state, props) => state.posts[props.id],
     (state, props) => state.postViews[props.id],
+
     (state, props) => {
       const authorId = state.posts[props.id].createdBy;
-      return state.users[authorId] || { id: authorId };
+      return state.users[authorId] && state.users[authorId].username;
     },
+    (state, props) => {
+      const authorId = state.posts[props.id].createdBy;
+      return state.users[authorId] && state.users[authorId].profilePictureLargeUrl;
+    },
+    (state, props) => {
+      const authorId = state.posts[props.id].createdBy;
+      return state.users[authorId] && state.users[authorId].profilePictureMediumUrl;
+    },
+
     (state) => state.me.id,
     getRecipientsRelatedThings,
     getPostAttachments
   ],
-  (post, postView, createdBy, myId, recipientsRelatedThings, attachments) => {
+  (post, postView, authorUsername, authorLargePic, authorMediumPic, myId, recipientsRelatedThings, attachments) => {
     if (!post) {
       return {};
     }
@@ -114,7 +124,11 @@ const makeGetPost = () => createSelector(
     return {
       ...post,
       ...postView,
-      createdBy,
+
+      authorUsername,
+      authorLargePic,
+      authorMediumPic,
+
       myId,
 
       recipients,

commit 9ba76e0aaf1c814a9daf02e84c636bd152bdc169

summer-cleaning: Fix post URL (regression after d21c83f)
During the recent refactoring (see d21c83f) I missed this occurrence of
createdBy, which caused the URLs of posts (posted to own feeds, not
groups) to contain "undefined" instead of username.

src/components/elements/post.jsx
@@ -242,7 +242,7 @@ class Post extends React.Component {
     // If all the recipients are groups (so it's not sent to author's feed,
     // or to some user as a direct message), use the first group as primary recipient.
     // Otherwise, use author's feed.
-    let primaryRecipient = props.createdBy;
+    let primaryRecipient = { username: props.authorUsername };
     if (props.recipients.every((recipient) => recipient.type === 'group')) {
       primaryRecipient = props.recipients[0];
     }