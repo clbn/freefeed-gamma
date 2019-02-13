clbn/freefeed-gamma

commit 8b0ec55556763ec88b207a4932f5de3335f18f8e

summer-cleaning: Remove unused prop in TileUserList
It's not needed after 6642ac0

src/components/elements/tile-user-list.jsx
@@ -143,7 +143,7 @@ class TileUserList extends React.Component {
 
     let usersData = props.users.map(user => {
       return {
-        ..._.pick(user, ['id', 'screenName', 'username', 'status', 'updatedAt']),
+        ..._.pick(user, ['id', 'username', 'status', 'updatedAt']),
         ...pickActions(config.type, props)
       };
     });

commit 9ba4fff021bfd53c8f8b14ef2474d0605fdd9d8c

Fix JS error in TileUserList (regression after 8b0ec55)
Removing "unused" prop in 8b0ec55 on 2018-07-25 was a mistake. Along
with `updatedAt` and `username`, the prop `screenName` is actually used
for sorting.

src/components/elements/tile-user-list.jsx
@@ -143,7 +143,7 @@ class TileUserList extends React.Component {
 
     let usersData = props.users.map(user => {
       return {
-        ..._.pick(user, ['id', 'username', 'status', 'updatedAt']),
+        ..._.pick(user, ['id', 'status', 'updatedAt', 'username', 'screenName']),
         ...pickActions(config.type, props)
       };
     });