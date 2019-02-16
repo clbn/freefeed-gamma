8eb03bc65929cc89a0f1367201cf8d0cad3c0ead
Alex Olshansky <i@creagenics.com>
2019-02-16 22:19:54 +0100

    Remove unnecessary exports in action-helpers.js

--- src/redux/action-helpers.js
+++ src/redux/action-helpers.js
@@ -9,16 +9,16 @@ export const request = (type) =>`${type}_REQUEST`;
 export const response = (type) => `${type}_RESPONSE`;
 export const fail = (type) => `${type}_FAIL`;
 
-export const feedGeneratingActions = [HOME, DISCUSSIONS, DIRECT, GET_SUMMARY, GET_SEARCH_RESULTS, GET_USER_FEED, GET_USER_SUMMARY, GET_USER_COMMENTS, GET_USER_LIKES];
-export const feedRequests = feedGeneratingActions.map(request);
-export const feedResponses = feedGeneratingActions.map(response);
-export const feedFails = feedGeneratingActions.map(fail);
+const feedGeneratingActions = [HOME, DISCUSSIONS, DIRECT, GET_SUMMARY, GET_SEARCH_RESULTS, GET_USER_FEED, GET_USER_SUMMARY, GET_USER_COMMENTS, GET_USER_LIKES];
+const feedRequests = feedGeneratingActions.map(request);
+const feedResponses = feedGeneratingActions.map(response);
+const feedFails = feedGeneratingActions.map(fail);
 export const isFeedRequest = action => feedRequests.indexOf(action.type) !== -1;
 export const isFeedResponse = action => feedResponses.indexOf(action.type) !== -1;
 export const isFeedFail = action => feedFails.indexOf(action.type) !== -1;
 
-export const userChangeActions = [SIGN_UP, SIGN_IN, WHO_AM_I, SUBSCRIBE, UNSUBSCRIBE, UPDATE_USER, UPDATE_FRONTEND_PREFERENCES];
-export const userChangeResponses = userChangeActions.map(response);
+const userChangeActions = [SIGN_UP, SIGN_IN, WHO_AM_I, SUBSCRIBE, UNSUBSCRIBE, UPDATE_USER, UPDATE_FRONTEND_PREFERENCES];
+const userChangeResponses = userChangeActions.map(response);
 export const isUserChangeResponse = action => userChangeResponses.indexOf(action.type) !== -1;
 
 export function requiresAuth(action) {
