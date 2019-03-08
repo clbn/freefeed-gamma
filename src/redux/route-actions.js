7c747f77a89578c981249fb1859f832a03b22ccd
Alex Olshansky <i@creagenics.com>
2019-03-09 00:41:16 +0100

    Add support for multiple actions per route

--- src/redux/route-actions.js
+++ src/redux/route-actions.js
@@ -17,29 +17,61 @@ import {
   getSinglePost
 } from './action-creators';
 
-const getOffset = nextRoute => +nextRoute.location.query.offset || 0; // "+" converts string to number
-
-const getSearchQuery = nextRoute => nextRoute.location.query.q || nextRoute.location.query.qs || '';
-
-const getUserName = nextRoute => nextRoute.params.userName;
+const offset = next => +next.location.query.offset || 0; // "+" converts string to number
+const q = next => next.location.query.q || next.location.query.qs || '';
+const username = next => next.params.userName;
+const days = next => next.params.days;
+const postId = next => next.params.postId;
 
 export const routeActions = {
-  'home': next => home(getOffset(next)),
-  'discussions': next => discussions(getOffset(next)),
-  'direct': next => direct(getOffset(next)),
+  'home': next => [
+    home(offset(next))
+  ],
+  'discussions': next => [
+    discussions(offset(next))
+  ],
+  'direct': next => [
+    direct(offset(next))
+  ],
+  'summary': next => [
+    getSummary(days(next))
+  ],
 
-  'summary': next => getSummary(next.params.days),
-  'search': next => getSearchResults(getSearchQuery(next), getOffset(next)),
-  'getUserInfo': next => getUserInfo(getUserName(next)),
-  'userFeed': next => getUserFeed(next.params.userName, getOffset(next)),
-  'userSummary': next => getUserSummary(next.params.userName, next.params.days),
-  'userComments': next => getUserComments(next.params.userName, getOffset(next)),
-  'userLikes': next => getUserLikes(next.params.userName, getOffset(next)),
-  'userSubscribers': next => getUserSubscribers(getUserName(next)),
-  'userSubscriptions': next => getUserSubscriptions(getUserName(next)),
-  'post': next => getSinglePost(next.params.postId)
+  'search': next => [
+    getSearchResults(q(next), offset(next))
+  ],
+
+  'getUserInfo': next => [
+    getUserInfo(username(next))
+  ],
+  'userFeed': next => [
+    getUserFeed(username(next), offset(next))
+  ],
+  'userSummary': next => [
+    getUserSummary(username(next), days(next))
+  ],
+  'userComments': next => [
+    getUserComments(username(next), offset(next))
+  ],
+  'userLikes': next => [
+    getUserLikes(username(next), offset(next))
+  ],
+  'userSubscribers': next => [
+    getUserSubscribers(username(next))
+  ],
+  'userSubscriptions': next => [
+    getUserSubscriptions(username(next))
+  ],
+
+  'post': next => [
+    getSinglePost(postId(next))
+  ]
 };
 
 export const bindRouteActions = dispatch => route => next => {
-  return dispatch(routeActions[route](next));
+  const actions = routeActions[route](next);
+
+  actions.forEach(a => {
+    dispatch(a);
+  });
 };
