674c4324727fc6bf138b39cfb105b3584289d810
Alex Olshansky <alex@creagenics.com>
2020-12-14 15:41:50 +0100

    Vote 2020
    Elections to the Supervisory Board in December 2020.
    
    This adds a temporary banner to the sidebar.

--- src/components/elements/sidebar.jsx
+++ src/components/elements/sidebar.jsx
@@ -9,6 +9,77 @@ import RecentGroups from './recent-groups';
 import Icon from "./icon";
 import Userpic from './userpic';
 
+const Vote2020 = ({ voteToken }) => {
+  if (!voteToken) {
+    return null;
+  }
+
+  return (
+    <div className="box alert alert-info" style={{ paddingTop: '10px' }}>
+      <div className="box-header">
+        Vote 2020
+      </div>
+      <div style={{ color: '#000', fontSize: '13px', paddingBottom: 0, borderBottom: '2px solid #000', marginBottom: '10px' }}>
+        <p>Elections to the <b>Supervisory Board</b> in December 2020</p>
+        <ul style={{ paddingLeft: '15px' }}>
+          <li><Link to={{ pathname: '/freefeed/da348e20-4075-4431-b4c8-6bd4a6e97e9a', query: {} }}
+            style={{ textDecoration: 'underline' }}>Details in English</Link></li>
+          <li><Link to={{ pathname: '/freefeed/6a64ded1-143f-4d11-9f7b-f9c8ac8b7f95', query: {} }}
+            style={{ textDecoration: 'underline' }}>Подробности на русском</Link></li>
+        </ul>
+      </div>
+
+      {voteToken === true ? (
+
+        <div style={{ color: '#000', fontSize: '13px', paddingBottom: 0 }}>
+          <p>
+            Voting will begin at<br/>
+            <b>15 Dec 2020, 12:00 MSK</b>
+          </p>
+          <p style={{ marginBottom: 0 }}>
+            Voting will end at<br/>
+            <b>16 Dec 2020, 12:00 MSK</b>
+          </p>
+        </div>
+
+      ) : voteToken === 'finished' ? (
+
+        <div style={{ color: '#000', fontSize: '13px', paddingBottom: 0 }}>
+          <p>Voting has ended.</p>
+          <p style={{ marginBottom: 0 }}>
+            The results will be published in{' '}
+            <Link to={{ pathname: '/freefeed', query: {} }} style={{ textDecoration: 'underline' }}>
+              @freefeed
+            </Link>
+            {' '}account.
+          </p>
+        </div>
+
+      ) : (
+
+        <div style={{ color: '#000', fontSize: '13px', paddingBottom: 0 }}>
+          <p>Voting has begun.</p>
+          <p>
+            Please vote{' '}
+            <b><a href="https://ffelection20.questionpro.com/" target="_blank" style={{ textDecoration: 'underline' }}>
+              using this link
+            </a></b>
+            {' '}with your unique vote key:
+          </p>
+          <p style={{ overflowWrap: 'break-word', wordWrap: 'break-word' }}>
+            <b>{voteToken}</b>
+          </p>
+          <p style={{ marginBottom: 0 }}>
+            Voting will end at <br/>
+            <b>16 Dec 2020, 12:00 MSK</b>
+          </p>
+        </div>
+
+      )}
+    </div>
+  );
+};
+
 const Sidebar = ({ me, signOut }) => {
   const bookmarkletRef = useRef(null);
 
@@ -74,6 +145,8 @@ const Sidebar = ({ me, signOut }) => {
             <Link to="/people">Browse/edit people</Link>
           </div>
         </div>
+
+        <Vote2020 voteToken={me.privateMeta?.vote2020}/>
 
         <div className="box">
           <div className="box-header">
